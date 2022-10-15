fd9ac07c8114ebd84f7f7335afc3a514188c197b
Jookie262 <barrientos.134038@kalibo.sti.edu.ph>
2022-10-16 00:09:40 +0800

    Create the UI, functionality and card component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import MovieCard from "./MovieCard";
+import "./App.css";
+import SearchIcon from "./search.svg";
+
+// The URL of the Site we want to fetch
+const API_URL = `http://www.omdbapi.com?apikey=${process.env.REACT_APP_OMDB_API_KEY}`
 
 const App = () => {
+
+    const [movies, setMovies] = useState();
+    const [searchTerm, setSearchTerm] = useState('');
+    
+    const searchMovies = async(title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        setMovies(data.Search);
+    };
+
+    useEffect(()=> {
+        searchMovies('');
+    },[]);
+    
     return (
-       <h1>Hello World</h1>
+       <div className="app">
+            <h1>MovieLand</h1>
+            <div className="search">
+                <input 
+                    placeholder="Search for Movies" 
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    onKeyPress = { 
+                        event => {
+                            if (event.key === 'Enter') {
+                                searchMovies(searchTerm) 
+                            }
+                        }
+                    }
+                />
+                <img 
+                    src={SearchIcon} 
+                    alt="Search"
+                    onClick={() => searchMovies(searchTerm)} 
+                />
+            </div>
+
+            {
+                movies?.length > 0 
+                ? (
+                    <div className="container">
+                        {movies.map((movie) => (<MovieCard movie={movie}/>))}
+                    </div>
+                ) : (
+                    <div className="empty">
+                        <h2>No Movies Found</h2>
+                    </div>
+                )
+            }
+       </div>
     );
 }
 
